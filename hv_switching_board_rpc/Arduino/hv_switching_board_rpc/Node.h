f927d944fcf5f67ec18ba6bc5538ec8ca391aecd
Christian Fobel <christian@fobel.net>
2014-05-06 13:45:03 -0400

    Update code templates from arduino_rpc...
    Update code templates from `arduino_rpc` revision 5ff500b.

--- hv_switching_board_rpc/Arduino/hv_switching_board_rpc/Node.h
+++ hv_switching_board_rpc/Arduino/hv_switching_board_rpc/Node.h
@@ -27,28 +27,11 @@ extern void i2c_request_event();
 class Node {
 public:
   uint32_t total_ram_size() { return ram_size(); }
-  uint32_t ram_data_size() { return data_size(); }
-  uint32_t ram_bss_size() { return bss_size(); }
-  uint32_t ram_heap_size() { return heap_size(); }
-  uint32_t ram_stack_size() { return stack_size(); }
   uint32_t ram_free() { return free_memory(); }
 
-  uint8_t high() const { return HIGH; }
-  uint8_t low() const { return LOW; }
-  uint8_t output() const { return OUTPUT; }
-  uint8_t input() const { return INPUT; }
-
   void pin_mode(uint8_t pin, uint8_t mode) { return pinMode(pin, mode); }
-  void delay_ms(uint32_t milliseconds) const { delay(milliseconds); }
   bool digital_read(uint8_t pin) const { return digitalRead(pin); }
   void digital_write(uint8_t pin, uint8_t value) { digitalWrite(pin, value); }
-  uint16_t analog_read(uint8_t pin) const { return analogRead(pin); }
-  void analog_write(uint8_t pin, uint8_t value) { return analogWrite(pin, value); }
-  uint32_t get_millis() const { return millis(); }
-  uint32_t get_micros() const { return micros(); }
-#ifndef DISABLE_I2C
-  int8_t forward_i2c_request(uint8_t address, uint8_t *request) const { return 0; }
-#endif  // #ifndef DISABLE_I2C
 
   void shift_register_reset() {
     /* Clear the state of the shift-register. */
@@ -63,7 +46,6 @@ public:
     digital_write(SS, HIGH);
   }
 
-#if 0
   void spi_write2(uint16_t two_bytes) {
     spi_write((two_bytes >> 8) & 0x0FF);
     spi_write(two_bytes & 0x0FF);
@@ -76,13 +58,12 @@ public:
 
   void spi_write5(uint32_t four_bytes, uint8_t byte_five) {
     digitalWrite(SS, LOW);
+    SPI.transfer(byte_five);
     for (int i = 24; i >= 0; i -= 8) {
       SPI.transfer((four_bytes >> i) & 0x0FF);
     }
-    SPI.transfer(byte_five);
     digitalWrite(SS, HIGH);
   }
-#endif
 };
 
 
--- hv_switching_board_rpc/Arduino/hv_switching_board_rpc/hv_switching_board_rpc.ino
+++ hv_switching_board_rpc/Arduino/hv_switching_board_rpc/hv_switching_board_rpc.ino
@@ -4,20 +4,11 @@
 #include "PacketParser.h"
 #include "Node.h"
 #include "CommandPacketHandler.h"
-//#include "NodeCommandProcessor.h"
-/* TODO: Re-enable `NodeCommandProcessor.h` include.
- * TODO: Modify `NodeCommandProcessor.h` code-generator template to include
- * request I2C forwarding.
- *  - This also requires an update to the protocol buffer generation to include
- *    the `ForwardI2cRequest` message types.
- *
- * __Temporarily__ disable include to prototype array argument handling through
- * Protocol Buffer RPC interface.  This will enable, among other things, acting
- * as a bridge between a serial connection and an i2c bus. */
-#include "CommandProcessor.h"
+#include "NodeCommandProcessor.h"
 #include "packet_handler.h"
 
-#define DISABLE_I2C
+//#define DISABLE_I2C
+//#define DISABLE_SERIAL
 
 #define PACKET_SIZE   24
 /* To save RAM, the serial-port interface may be disabled by defining
